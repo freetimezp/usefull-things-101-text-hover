839f8456556e7d56f60f0bff643138992640c5f2
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-26 17:13:10 +0300

    03. usefull-things-101-text-hover. Bg, split text.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const textContainers = document.querySelectorAll(".word");
 
-const textContainers = document.querySelectorAll(".word");
-
-const defaultScale = 1;
-const maxScale = 2;
-const neighborScale = 1.5;
-
-textContainers.forEach((textContainer) => {
-    const spans = textContainer.querySelectorAll("span");
-
-    textContainer.addEventListener("mousemove", (e) => {
-        const target = e.target;
-
-        const index = Array.from(spans).indexOf(target);
-
-        spans.forEach((span, i) => {
-            let scale = defaultScale;
-
-            if (i === index) {
-                scale = maxScale;
-            } else if (i === index - 1 || i === index + 1) {
-                scale = neighborScale;
-            }
-
-            span.style.transform = `scaleY(${scale})`;
-        });
+    textContainers.forEach((textWrapper) => {
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
     });
 
-    textContainer.addEventListener("mouseleave", () => {
-        spans.forEach((span) => {
-            span.style.transform = `scaleY(${defaultScale})`;
+    const defaultScale = 1;
+    const maxScale = 2;
+    const neighborScale = 1.5;
+
+    textContainers.forEach((textContainer) => {
+        const spans = textContainer.querySelectorAll("span");
+
+        textContainer.addEventListener("mousemove", (e) => {
+            const target = e.target;
+
+            const index = Array.from(spans).indexOf(target);
+
+            spans.forEach((span, i) => {
+                let scale = defaultScale;
+
+                if (i === index) {
+                    scale = maxScale;
+                } else if (i === index - 1 || i === index + 1) {
+                    scale = neighborScale;
+                }
+
+                span.style.transform = `scaleY(${scale})`;
+            });
+        });
+
+        textContainer.addEventListener("mouseleave", () => {
+            spans.forEach((span) => {
+                span.style.transform = `scaleY(${defaultScale})`;
+            });
         });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
